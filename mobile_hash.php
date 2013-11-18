e4f0209acf9ba74f94631d0b5a3e1a343afb13d6
VinodKumarLogan <vinodkumarlogan@gmail.com>
2013-11-19 01:07:29 +0530

    updated mobile hash app

--- mobile_hash.php
+++ mobile_hash.php
@@ -1,8 +1,8 @@
  <html>
     <head>
         <title> To get the hash value of a mobile number</title>
-            <meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
-            <meta name='txtweb-appkey' content='fc377313-b42f-460b-a2bd-08b39e6ed8eb' />
+            <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'/>
+            <meta name='txtweb-appkey' content=' fc377313-b42f-460b-a2bd-08b39e6ed8eb'/>
     </head>
     <body>
         <?php
